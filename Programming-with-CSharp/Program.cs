b3ff8047ceb93c3c28d580bdcebbecc5346cfa18
Boris Breuer <borisbreuer1@gmail.com>
2015-06-07 14:05:04 +0200

    Added module 1 assignment
    1. Create a C# Console application.
    
    2.  Within the Main() method in this application, create variables of
    the correct data type for the information related to a student only.
    The other information will be used in later modules when you create
    class files for the other objects listed:
    
    Student Information
    
    First Name Last Name Birthdate Address Line 1 Address Line 2 City
    State/Province Zip/Postal Country

--- Programming-with-CSharp/Program.cs
+++ Programming-with-CSharp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Programming_with_CSharp
 {
@@ -10,6 +6,39 @@ namespace Programming_with_CSharp
     {
         static void Main(string[] args)
         {
+            string firstName;
+            string lastName;
+            DateTime birthDate;
+            string addressLine1;
+            string addressLine2;
+            string city;
+            string stateProvince;
+            string zipPostal;
+            string country;
+
+            firstName = "Biff";
+            lastName = "the Understudy";
+            birthDate = DateTime.ParseExact(
+                "1995-01-01 00:00:00,000",
+                "yyyy-MM-dd HH:mm:ss,fff",
+                System.Globalization.CultureInfo.InvariantCulture);
+            addressLine1 = "Infinity loop 1";
+            addressLine2 = "Infinity loop 3";
+            city = "Cubertino";
+            stateProvince = "California";
+            zipPostal = "CA 95014";
+            country = "United States";
+
+            Console.WriteLine("First name: " + firstName);
+            Console.WriteLine("Last name: " + lastName);
+            Console.WriteLine("Birth date: " + birthDate.ToString());
+            Console.WriteLine("Address line 1: " + addressLine1);
+            Console.WriteLine("Address line 2: " + addressLine2);
+            Console.WriteLine("City: " + city);
+            Console.WriteLine("State/province: " + stateProvince);
+            Console.WriteLine("Zip/postal code: " + zipPostal);
+            Console.WriteLine("Country: " + country);
+            Console.ReadLine();
         }
     }
 }
