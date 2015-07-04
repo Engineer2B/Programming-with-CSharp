fff73c1ed5eccc7247df190c0c13fc2042b68bc2
Boris Breuer <borisbreuer1@gmail.com>
2015-07-04 19:06:53 +0200

    :loudspeaker: Finished module 2. :heavy_plus_sign: problem description for module 1 and 2.

--- Module-1/Program.cs
+++ Module-1/Program.cs
@@ -1,5 +1,65 @@
 ﻿using System;
 
+/*
+NOTE: In this course you will create variables for classes in the application. 
+For now, the class definitions are not included. 
+In later modules, you will move this code into separate class files to aid in code reuse.
+This assignment just lets you focus on data types, statements, etc. in C# to ease you into the language.
+
+We'll use a common application theme throughout the course for your assignments.
+The assignments will provide hands-on coding tasks to create application components
+that could form a complete application when the course is finished.
+
+For this module, you have learned about the data types, operators, and statements in C#.
+
+Using this knowledge you will start to form some of the foundation for your application.
+This assignment is designed to allow you to focus on the data types that are appropriate for
+attributes of real-world objects. Later in the course, you will start to combine these attributes
+with behaviors (methods) and then transition them into object-oriented classes.
+
+Also, this assignment offers suggestions for attributes for each object but does
+not give explicit instructions on every field required. You will be required to think through
+the attributes of these "objects" as you create the necessary data to support them in an application.
+Your list may be different in an application that you would create on your own.
+
+1.  Create a C# Console application.
+
+2.  Within the Main() method in this application, create variables of the correct data type
+for the information related to a student only.  The other information will be used in later modules
+when you create class files for the other objects listed:
+
+Student Information
+
+First Name	Last Name	Birthdate	Address Line 1	Address Line 2	City	State/Province	Zip/Postal	Country
+Teacher Information
+
+First Name	Last Name	Birthdate	Address Line 1	Address Line 2	City	State/Province	Zip/Postal	Country
+UProgram Information
+
+Program Name	Department Head	Degrees
+Degree Information
+
+Degree Name	Credits Required
+Course Information
+
+Course Name	Credits	Duration in Weeks	Teacher
+3.  Once you have the variables created, use assignment statements to assign values
+to one set of student variables and use the Console.WriteLine() method to output the values to the console window.
+
+Again, this assignment is merely intended to check your understanding of how to create variables,
+assign values to them, and output the information to a console window. 
+You will build on these concepts and begin to create more functionality in later modules.
+
+4.  Post your code in the peer review area for feedback from other students.
+
+Challenge (Not graded)
+
+Investigate the .NET Framework documenation around Console.ReadLine() and modify your
+code to use this method for accepting input from a user of your application.
+Using Console.ReadLine(), prompt a user for information about a student.
+One prompt for each student variable you created earlier.
+Use the appropriate code to assign the values from the user to the variables for the student.
+*/
 namespace Programming_with_CSharp
 {
     class Program
--- Module-2/Program.cs
+++ Module-2/Program.cs
@@ -1,15 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/*
+For this assignment, you will create the pattern of a chess board that is 8 x 8.
+Use X and O to represent the squares.
+
+Create the appropriate nested looping structure to output the characters in an 8 x 8 grid
+on the screen using Console.Write() or Console.WriteLine() as appropriate.
+Include a decision structure to ensure that alternate rows start with opposite characters
+as a real chess board alternates the colors among rows.
+This is what your output should look like.
+
+XOXOXOXO
+OXOXOXOX
+XOXOXOXO
+OXOXOXOX
+XOXOXOXO
+OXOXOXOX
+XOXOXOXO
+OXOXOXOX
+
+Grading Criteria:
+
+1. Used a nested loop
+2. Used a decision structure to flip row output
+3. Output is correct per above image
+*/
 namespace Module_2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            for (int indexRow = 1; indexRow <= 8; indexRow++)
+            {
+                for (int indexColumn = 1; indexColumn <= 8; indexColumn++)
+                {
+                    if (indexRow % 2 == 0)
+                    {
+                        if (indexColumn % 2 == 0)
+                        {
+                            Console.Write('X');
+                        }
+                        else
+                        {
+                            Console.Write('O');
+                        }
+                    }
+                    else
+                    {
+                        if (indexColumn % 2 == 0)
+                        {
+                            Console.Write('O');
+                        }
+                        else
+                        {
+                            Console.Write('X');
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
